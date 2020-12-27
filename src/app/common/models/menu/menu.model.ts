ea68a0249b8ac2919cf7884cd9103d6be52e4ccd
Hugo Deiró <hugodeiro@gmail.com>
2020-12-27 19:03:00 -0300

    Filtering menu Items per Feature Flag

--- src/app/common/models/menu/menu.model.ts
+++ src/app/common/models/menu/menu.model.ts
@@ -1,6 +1,7 @@
 export class Menu {
   name:  string;
   route: string;
+  featureFlag: string;
 
   static build(payload) {
     const item = new Menu();
--- src/app/common/services/menu/menu.service.ts
+++ src/app/common/services/menu/menu.service.ts
@@ -1,3 +1,5 @@
+import { environment } from './../../../../environments/environment.prod';
+import { map } from 'rxjs/operators';
 import { Menu } from './../../models/menu/menu.model';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
@@ -6,8 +8,10 @@ import { Observable, of } from 'rxjs';
 export class MenuService {
   getMenus(): Observable<Menu[]> {
     return of([
-      Menu.build({name: 'app.menu.item-1', route: 'home'}),
-      Menu.build({name: 'app.menu.item-2', route: 'about'})
-    ]);
+      Menu.build({name: 'app.menu.item-1', route: 'home', featureFlag: 'home'}),
+      Menu.build({name: 'app.menu.item-2', route: 'about', featureFlag: 'about'})
+    ]).pipe(
+      map(item => item.filter(menu => menu.featureFlag && environment?.featureFlags[menu.featureFlag]))
+    );
   }
 }
